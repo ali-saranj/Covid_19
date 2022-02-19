0f32e261d946238bec680c4ccb45b38c83ad2cd7
Acer <ali.sasranj1384@gmail.com>
2022-02-19 01:58:00 -0800

    add translate

--- app/src/main/java/com/example/covid_19/AboutMe.kt
+++ app/src/main/java/com/example/covid_19/AboutMe.kt
@@ -1,14 +1,12 @@
 package com.example.covid_19
 
 import android.Manifest
-import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
